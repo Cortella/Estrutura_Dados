c373c8844680da26d798d4de09f190390d76c764
Cortella <nicolau.cortella@gmail.com>
2019-11-05 18:57:45 -0300

    Inicio Tp_2

---
+++ TP_2/main.cpp
@@ -0,0 +1,18 @@
+//main
+
+//Inclusao de bicliotecas
+#include <iostream>
+#include <stdlib.h>
+
+//Uso de recursos e namespaces
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+
+int main(){
+
+    
+    system("pause")
+    return ;
+}
