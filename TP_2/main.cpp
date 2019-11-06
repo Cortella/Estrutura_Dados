a8b1bdfc34affb2de8a50fbfd240838db7d83c4d
Cortella <nicolau.cortella@gmail.com>
2019-11-06 04:30:39 -0300

    Inicio TP_2

--- TP_2/main.cpp
+++ TP_2/main.cpp
@@ -5,14 +5,23 @@
 #include <stdlib.h>
 
 //Uso de recursos e namespaces
-using std::cout;
-using std::cin;
-using std::endl;
-using std::string;
+using namespace std;
 
 int main(){
-
+    int T,P,x;
+    cin >> T >> P >> x;
+    for(int i=0;i<P;i++){
+        cin 
+    }
+    char nome[x+1];
+    for(int i=0; i<x;i++){
+        cin >> nome[i];
+        if(i==4){
+            nome[x] = '\0';
+        }
+    }
     
-    system("pause")
-    return ;
+    
+    system("pause");
+    return 0;
 }
---
+++ TP_2/teste.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+
+using namespace std;
+
+int main(){
+
+    string x = "ol";
+    cout<< x.size();
+    system("pause");
+    return 0;
+}
